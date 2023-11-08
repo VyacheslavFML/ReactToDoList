5106609a7cfc9f3592cb7d1e18c7945a1cbbbfbd
KimFML <vkim300405@gmail.com>
2023-11-08 23:05:24 +0600

    ReactToDoList
    ReactToDoList

---
+++ react-todolist2/src/App.js
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import ToDo from './ToDo';
+import ToDoForm from './ToDoForm';
+
+function App() {
+    const [todos, setTodos] = useState([]);
+    const addTask=(userInput) => {
+        if (userInput) {
+            const newItem={
+                id:Math.random().toString(36).substring(2,9),
+                task:userInput,
+                completed:false
+            }
+            setTodos([...todos, newItem])
+        }
+    };
+
+    const removeTask=(id) => {
+      setTodos([...todos.filter((todo) => todo.id !== id)])
+    };
+
+    const handleToggle=(id) => {
+     setTodos([...todos.map((todo) => 
+    todo.id === id ? {...todo, complete: !todo.completed } : {...todo})
+    ])
+    }
+
+  return (
+    <div className="App">
+      <header>
+        <h1>ToDo List: {todos.length}</h1>
+      </header>
+      <ToDoForm addTask={addTask}/>
+      {todos.map((todo)  =>{
+        return (
+        <ToDo
+        todo={todo}
+        key={todo.id}
+        toggleTask={handleToggle}
+        removeTask={removeTask}
+        />
+      )
+    })}
+    </div>
+  );
+}
+
+export default App;
---
+++ react-todolist2/src/ToDoForm.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+function ToDoForm({addTask}) {
+    const [userInput, setUserInput] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        addTask(userInput)
+        setUserInput("")
+     
+    }
+    const handleChange = (e) => {
+          setUserInput(e.currentTarget.value)
+    }
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            handleSubmit(e)
+        }
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input 
+            value={userInput}
+            type="text"
+            onChange={handleChange}
+            onKeyDown={handleKeyPress}
+            placeholder='What is your task?'
+            />
+            <button>Save</button>
+        </form>
+    )
+}
+
+export default ToDoForm;
